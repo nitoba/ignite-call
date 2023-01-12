7cad2e1786c116346ac2d431cd239adf48c81dd1
Bruno Alves <nito.ba.dev@gmail.com>
2023-01-11 21:37:10 -0300

    feat: add axios && react query

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,8 +1,14 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
+import { queryClient } from '../lib/react-query'
 import { globalStyles } from '../styles/global'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
